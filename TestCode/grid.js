d25bd7c6fdb65673f364c9ae4423dd7dd6723e49
Randy Steinkamp <randy118@utexas.edu>
2020-03-30 18:27:28 -0500

    add some grid functionality

--- TestCode/grid.js
+++ TestCode/grid.js
@@ -32,19 +32,20 @@ class grid {
     //check mouse pos
     
     //with mouseState
-    findRelativeBox(curX, curY){ 
+    findRelativeBox(curX, curY){
         //find column
+        var initSpace = 15;
         var column = -1;
         var row = -1;
         for(var x = 0; x < this.boxNumberX; x++){
-            if(curX > x*this.scale && curx <= x+1*this.scale){
+            if(curX - initSpace > x*this.scale && curX - initSpace <= (x+1)*this.scale){
                 column = x;
             }
         }
         //find row
         if(column != -1){ //if mouse isn't in canvas, no need to check
             for(var y = 0; y < this.boxNumberY; y++){
-                if(curY > y*this.scale && curY <= y+1*this.scale){
+                if(curY - initSpace > y*this.scale && curY - initSpace <= (y+1)*this.scale){
                     row = y;
                 }
             }
@@ -54,13 +55,13 @@ class grid {
     
     drawBoxAtPosition(curX, curY, color) { //assume already down
         var curPosition = this.findRelativeBox(curX, curY); //find which box we are dealing with
+//        console.log("Is here");
         if(curPosition[0] != -1 && curPosition[1] != -1){
             this.ctx.beginPath();
-            this.ctx.fillStyle(color); //color
-            this.ctx.fillRect(curPosition[0]*this.scale, curPosition[1]*this.scale, this.scale, this.scale);
+            this.ctx.fillStyle = color; //color
+            this.ctx.fillRect(curPosition[1]*this.scale, curPosition[0]*this.scale, this.scale, this.scale);
             this.colorArray[curPosition[0] + curPosition[1]] = color;
             this.ctx.stroke();
-            console.log("Is here");
         }
     }
 }
@@ -74,10 +75,14 @@ document.onmousedown = mouseDown;
 document.onmouseup = mouseUp;
 var mouseState = "up";
 
+//get mouseX, mouseY
+var mouseX = 0;
+var mouseY = 0;
 function mouseMove(e) {
+    mouseX = e.screenX;
+    mouseY = e.screenY;
     if(mouseState == "down") {
-        grid1.drawBoxAtPosition(tempX, tempY, "#000000");
-        console.log(tempX, tempY);
+        grid1.drawBoxAtPosition(mouseX, mouseY, "#000000");
         //mouse is being clicked
     }
     
@@ -94,11 +99,3 @@ function mouseUp(e) {
     mouseState = "up";
 }
 
-//get mouseX, mouseY
-var tempX = 0;
-var tempY = 0;
-
-function mouseXY(e) {
-    tempX = e.screenX;
-    tempY = e.screenY;
-}
