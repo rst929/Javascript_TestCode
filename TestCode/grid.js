8e7996d64d9bb5430ced64205d0a370f3750f41d
Randy Steinkamp <randy118@utexas.edu>
2020-03-30 10:22:34 -0500

    clean code

--- TestCode/grid.js
+++ TestCode/grid.js
@@ -1,21 +1,12 @@
-//var c = document.getElementById("myCanvas");
-//var ctx = c.getContext("2d");
-//ctx.beginPath();
-//ctx.rect(50, 20, 150, 100);
-//ctx.stroke()
-//
-
 class grid {
     constructor(width, height, ctx) {
         this.width = width;
         this.height = height;
         this.ctx = ctx;
-        //grid length/height
     }
     
     drawBox(boxNumberX, boxNumberY) {
         var scale = Math.min(this.width/boxNumberX, this.height/boxNumberY);
-//        var scale = this.width/boxNumber;
         var i, j;
         var startHeight = 0;
         for (i = 0; i < boxNumberY; i++) {
@@ -33,6 +24,5 @@ class grid {
 
 var c = document.getElementById("myCanvas");
 var ctx = c.getContext("2d");
-//var grid1 = new grid(ctx.width, ctx.height, ctx);
 var grid1 = new grid(500, 150, ctx);
 grid1.drawBox(10, 10);
